fd619a9c6b2e120982c54aaf0def5ab7b08d5082
helplessStruggle <kresco96@gmail.com>
2015-12-25 18:26:11 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -5,12 +5,22 @@ class Board
 {
 public:
 	Piece *board[8][8];
+	Piece *getPiece(int x, int y) { return Board::board[y][x]; }
+	//returns a pointer to a piece
 	 Board();
 	~Board();
 	std::string toString();
-	bool isValid(int, int, bool, int, int);
-	bool movePiece(int, int, bool, int, int);
+	bool isValid(int, int, int, int, bool);
+	bool movePiece(int, int, int, int, bool);
 	bool isKingValid(int, int, bool);
-	bool eat(int, int, bool,int, int);
+	bool eat(int, int, int, int, bool);
 	bool gameOver();
+	bool checkCanEat(int, int, bool);
+	bool isCoordValid(int, int);
+	bool checkBlank(int, int, bool);
+	bool eat2(int i, int j, int dest_x, int dest_y, bool color);
+
+private:
+	int whitePiece = 0;
+	int redPiece = 0;
 };
